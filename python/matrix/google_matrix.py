c4ffb85f654c8a5389b28f05122b9b70e8ef3149
Ben <ruubixo@gmail.com>
2019-06-19 20:37:08 -0400

    Completed matrix.py challenge.

---
+++ python/matrix/google_matrix.py
@@ -0,0 +1,14 @@
+'''
+https://sites.google.com/site/usfcomputerscience/matrices
+'''
+
+
+matrix = [
+          [5, 4, 7, 11],
+          [3, 3, 8, 17],
+          [4, 5, 9, ]
+          ]
+
+for row in matrix:
+   for column in row:
+       print(column)
--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,42 @@
+'''
+Given a string representing a matrix of numbers, return the rows and columns of
+that matrix.
+
+So given a string with embedded newlines like:
+
+```text
+9 8 7
+5 3 2
+6 6 7
+```
+
+representing this matrix:
+
+```text
+    1  2  3
+  |---------
+1 | 9  8  7
+2 | 5  3  2
+3 | 6  6  7
+```
+
+your code should be able to spit out:
+
+- A list of the rows, reading each row left-to-right while moving
+  top-to-bottom across the rows,
+- A list of the columns, reading each column top-to-bottom while moving
+  from left-to-right.
+'''
+
+
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.rows = [[int(n) for n in row.split()]
+                     for row in matrix_string.split('\n')]
+        self.columns = [list(tup) for tup in zip(*self.rows)]
 
     def row(self, index):
-        pass
+        return self.rows[index - 1]
 
     def column(self, index):
-        pass
+        return self.columns[index - 1]
